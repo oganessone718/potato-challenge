1a6e45c204beda100caa84efe9a1766c51720059
oganessone718 <oganessone718@gmail.com>
2023-05-19 20:41:59 +0900

    Using tsx

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -6,9 +6,17 @@ import { useNavigate }  from "react-router-dom";
 const HomePage = (props: {}) => {
   const navigate = useNavigate();
 
+  const testModel = () => {
+    const asyncFun = async () => {
+      await axios.post( "http://localhost:8080" + '/challenge/testChallenge', {  } );
+    }
+    asyncFun().catch(e => window.alert(`AN ERROR OCCURED! ${e}`));
+  }
+
   return (
     <div className={"home"}>
       I have nothing~
+      <div className={"button"} onClick={(e) => testModel()}>test</div>
     </div>
   )
 };
